8088b1260d30c2333ecfb3090ee71e5fc89ac2c0
jijog-jijog <jijo.cs22@stellamaryscoe.edu.in>
2025-07-22 19:41:40 +0530

    Fix formatting by adding blank lines for better readability

--- num1.java
+++ num1.java
@@ -1,10 +1,12 @@
 public class num1 {
     public static void main(String[] args) {
+        
         int n =  11;
+
         bitcount(n);
     }
     static int bitcount(int n){
-        
+
         if (n > 1) {
 
 
